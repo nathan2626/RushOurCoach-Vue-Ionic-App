17ab87343339e63ac132b89929dae642e255c732
nathan2626 <61419599+nathan2626@users.noreply.github.com>
2021-03-05 17:35:10 +0100

    Home page styling almost complete

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,68 +1,232 @@
 <template>
   <ion-page>
-    <ion-header :translucent="true">
+    <ion-header :translucent="true" class="ion-no-border">
       <ion-toolbar>
-        <ion-title>Blank</ion-title>
+
+        <ion-buttons slot="start">
+          <ion-button>
+            <ion-icon slot="end" :icon="gridOutline"></ion-icon>
+          </ion-button>
+        </ion-buttons>
+        <ion-buttons slot="end">
+          <ion-button>
+            <ion-icon slot="start" :icon="search"></ion-icon>
+          </ion-button>
+        </ion-buttons>
+
       </ion-toolbar>
     </ion-header>
-    
+
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
-    
       <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+        <!-- Content -->
+        <ion-segment scrollable value="1">
+          <ion-segment-button value="1">
+            <ion-label>Categorie 1</ion-label>
+          </ion-segment-button>
+          <ion-segment-button value="2">
+            <ion-label>Categorie 1</ion-label>
+          </ion-segment-button>
+          <ion-segment-button value="3">
+            <ion-label>Categorie 1</ion-label>
+          </ion-segment-button>
+          <ion-segment-button value="4">
+            <ion-label>Categorie 1</ion-label>
+          </ion-segment-button>
+        </ion-segment>
+
+        <div class="ion-padding">
+          <ion-grid>
+            <ion-row>
+              <ion-col size="3">
+                <ion-badge color="light">Boxe</ion-badge>
+              </ion-col>
+              <ion-col size="3">
+                <ion-badge color="light">MMA</ion-badge>
+              </ion-col>
+              <ion-col size="3">
+                <ion-badge color="light">Krav</ion-badge>
+              </ion-col>
+              <ion-col size="3">
+                <ion-badge color="light">Judo</ion-badge>
+              </ion-col>
+            </ion-row>
+          </ion-grid>
+        </div>
+
+        <ion-slides :options="slideOpts">
+
+          <ion-slide>
+            <ion-grid>
+              <ion-row>
+                <ion-col size="12" class="poster-col">
+                  <img class="poster" src="https://www.wallpapertip.com/wmimgs/84-843693_nature-wallpaper-hd-vertical.jpg" alt="">
+                </ion-col>
+                <ion-col size="12">
+                  <h5 class="ion-no-margin">Coach untel</h5>
+                  <p class="ion-no-margin rate">
+                    <ion-icon :icon="star"></ion-icon>4.5
+                  </p>
+                </ion-col>
+              </ion-row>
+            </ion-grid>
+          </ion-slide>
+
+          <ion-slide>
+            <ion-grid>
+              <ion-row>
+                <ion-col size="12" class="poster-col">
+                  <img class="poster" src="https://www.wallpapertip.com/wmimgs/84-843693_nature-wallpaper-hd-vertical.jpg" alt="">
+                </ion-col>
+                <ion-col size="12">
+                  <h5 class="ion-no-margin">Coach untel</h5>
+                  <p class="ion-no-margin rate">
+                    <ion-icon :icon="star"></ion-icon>4.5
+                  </p>
+                </ion-col>
+              </ion-row>
+            </ion-grid>
+          </ion-slide>
+
+          <ion-slide>
+            <ion-grid>
+              <ion-row>
+                <ion-col size="12" class="poster-col">
+                  <img class="poster" src="https://www.wallpapertip.com/wmimgs/84-843693_nature-wallpaper-hd-vertical.jpg" alt="">
+                </ion-col>
+                <ion-col size="12">
+                  <h5 class="ion-no-margin">Coach untel</h5>
+                  <p class="ion-no-margin rate">
+                    <ion-icon :icon="star"></ion-icon>4.5
+                  </p>
+                </ion-col>
+              </ion-row>
+            </ion-grid>
+          </ion-slide>
+
+        </ion-slides>
+
       </div>
     </ion-content>
   </ion-page>
 </template>
 
 <script>
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import { IonContent, IonSegment, IonSegmentButton, IonLabel, IonSlides, IonSlide, IonGrid, IonRow, IonCol, IonBadge,  IonButtons, IonButton, IonIcon, IonHeader, IonPage, IonToolbar } from '@ionic/vue';
+import { search, gridOutline, star } from 'ionicons/icons';
 import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'Home',
   components: {
     IonContent,
+    IonSlides,
+    IonSlide,
+    IonSegment,
+    IonGrid,
+    IonRow,
+    IonCol,
+    IonBadge,
+    IonSegmentButton,
+    IonLabel,
+    IonButtons,
+    IonButton,
+    IonIcon,
     IonHeader,
     IonPage,
-    IonTitle,
     IonToolbar
+  },
+  setup() {
+    const slideOpts = {
+      centeredSlides: true,
+      loop: true,
+      spaceBetween: -100
+    };
+    return {
+      search,
+      gridOutline,
+      star,
+      slideOpts
+    }
   }
 });
 </script>
 
 <style scoped>
-#container {
-  text-align: center;
-  
+
+ion-segment {
+  margin: 0 10px;
+}
+
+ion-segment-button {
+  text-transform: capitalize;
+  transform: scale(.8, .8);
+  --indicator-color: transparent;
+  color: #969696;
+}
+
+ion-segment-button ion-label {
+  position: relative;
+  left: -17px;
+  font-weight: bolder;
+  font-size: 18px;
+}
+
+.segment-button-checked {
+  color: #333;
+}
+
+.segment-button-checked:before {
+  content: "";
   position: absolute;
   left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
+  bottom: 0;
+  width: 50%;
+  border-bottom: 4px solid #333;
 }
 
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
+ion-badge {
+  padding: 8px 0;
+  border-radius: 10px;
   color: #8c8c8c;
-  
-  margin: 0;
+  border: 1px solid #8c8c8c;
+  width: 100%;
+  text-align: center;
 }
 
-#container a {
-  text-decoration: none;
+.poster-col {
+  height: 26em;
 }
+.poster-col img {
+  border-radius: 40px;
+}
+
+.rate {
+  font-size: 14px;
+  padding-top: 4px;
+}
+
+.rate ion-icon {
+  color: gold;
+}
+
+h5 {
+  margin-top: 4px;
+}
+
+/* Animations */
+.swiper-slide-prev {
+  opacity: .5;
+  transform: rotate(-8deg);
+}
+.swiper-slide-next {
+  opacity: .5;
+  transform: rotate(8deg);
+}
+.swiper-slide {
+  transition: .4s all;
+}
+
+
+
 </style>
